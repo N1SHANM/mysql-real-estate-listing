ba84cd2b7c967142d0fae41764a5c3ffa880a69e
N1SHANM <140622382+N1SHANM@users.noreply.github.com>
2023-10-15 20:50:00 +0000

    added database.py

---
+++ database.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine
+
+engine=create_engine("mysql+pymysql://a73vxqrjvliq6msjdphi:pscale_pw_jhsppLL4m5WQGdTk3AliXL9qtyUsWB89Dej1QOQVD4@aws.connect.psdb.cloud/real_estate_listings?charset=utf8mb4")
