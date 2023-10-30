dfb022f70ec1579bdc0b8aa1f6fbe93873a22ca0
N1SHANM <140622382+N1SHANM@users.noreply.github.com>
2023-10-30 01:18:30 +0000

    used alchemy

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+from database import engine
+from sqlalchemy import text
 
 app=Flask(__name__)
 
+def load_buyer_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from buyer"))
+    buyers = []
+    for rows in result.all():
+      buyers.append(dict(row))
+  
+
+
 @app.route("/")
 def hello_estates():
-  return render_template('home.html')
+  return render_template('home.html'
+                        jobs=JOBS)
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -1,3 +1,12 @@
 from sqlalchemy import create_engine
 
-engine=create_engine("mysql+pymysql://a73vxqrjvliq6msjdphi:pscale_pw_jhsppLL4m5WQGdTk3AliXL9qtyUsWB89Dej1QOQVD4@aws.connect.psdb.cloud/real_estate_listings?charset=utf8mb4")
+db_connection_string = "mysql+pymysql://pscale_pw_KhSMxIK1SvGsuDds53TAdbMsr1ovLvO3hCxQZuoNRgC:pscale_pw_KhSMxIK1SvGsuDds53TAdbMsr1ovLvO3hCxQZuoNRgC@aws.connect.psdb.cloud/real_estate_listings?charset=utf8mb4"
+
+engine=create_engine(
+  db_connection_string,
+  connect_args={
+    ssl      = {
+      "ssl ca": "/etc/ssl/cert.pem"
+    }      
+  })
+                     
