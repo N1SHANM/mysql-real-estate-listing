95abe7b52fd462bcdc16570d7d937d3fab951dac
N1SHANM <140622382+N1SHANM@users.noreply.github.com>
2023-11-09 19:20:07 +0000

    created propertylist.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from database import engine
 from sqlalchemy import text
 
@@ -6,17 +6,28 @@ app=Flask(__name__)
 
 def load_buyer_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from buyer"))
-    buyers = []
+    result = conn.execute(text("select * from property"))
+    property = []
     for rows in result.all():
-      buyers.append(dict(row))
+      property.append(dict(row))
+    return property
   
 
 
 @app.route("/")
 def hello_estates():
-  return render_template('home.html'
-                        jobs=JOBS)
+  return render_template('home.html',property=property)
+
+@app.route("/properties")
+def list_jobs():
+  return jsonify(JOBS)
+
+@app.route("/properties/<id>")
+def show_jobs(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not found",404
+  return render_template('jobpage.html',job=job)
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -1,12 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://pscale_pw_KhSMxIK1SvGsuDds53TAdbMsr1ovLvO3hCxQZuoNRgC:pscale_pw_KhSMxIK1SvGsuDds53TAdbMsr1ovLvO3hCxQZuoNRgC@aws.connect.psdb.cloud/real_estate_listings?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://bv4gc0428pe25v0qovkm:pscale_pw_KzGYy90sgYHkCSikYCJZOeAc69IkpyedWWPzpUXoabs@aws.connect.psdb.cloud/qwerty?charset=utf8mb4"
 
 engine=create_engine(
   db_connection_string,
   connect_args={
-    ssl      = {
+    "ssl"      : {
       "ssl ca": "/etc/ssl/cert.pem"
     }      
   })
-                     
+
+with engine.connect() as conn:
+  result = conn.execute(text("select * from property"))
+  print(result.all())
+  
+
+          
